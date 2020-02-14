14d37515f2441695d327086134bb744f2392a6fd
maachang <maachang@gmail.com>
2020-02-14 16:36:02 +0900

    JavaWrapper系の処理対応

---
+++ project/src/rhigin/scripts/RhiginWrapUtil.java
@@ -0,0 +1,139 @@
+package rhigin.scripts;
+
+import java.lang.reflect.Method;
+
+import org.mozilla.javascript.IdScriptableObject;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.Undefined;
+import org.mozilla.javascript.Wrapper;
+
+import rhigin.RhiginException;
+import rhigin.scripts.objects.JDateObject;
+import rhigin.util.Converter;
+
+/**
+ * javascriptからのjavaオブジェクトのラップ・アンラップ支援.
+ */
+public class RhiginWrapUtil {
+	protected RhiginWrapUtil() {}
+	
+	/**
+	 * Javaのパラメータとしてアンラップ.
+	 * @param args
+	 * @return
+	 */
+	public static final Object[] unwrapArgs(final Object[] args) {
+		final int length = args == null ? 0 : args.length;
+		if(length == 0) {
+			return args;
+		}
+		for(int i = 0; i < length; i ++) {
+			args[i] = unwrap(args[i]);
+		}
+		return args;
+	}
+	
+	/**
+	 * Javaのパラメータとしてアンラップ.
+	 * @param off
+	 * @param args
+	 * @return
+	 */
+	public static final Object[] unwrapArgs(final int off, final Object[] args) {
+		final int length = args == null ? 0 : args.length;
+		if(length == 0) {
+			return args;
+		}
+		if(off == 0) {
+			for(int i = 0; i < length; i ++) {
+				args[i] = unwrap(args[i]);
+			}
+			return args;
+		}
+		int cnt = 0;
+		final Object[] pms = new Object[length - off];
+		for(int i = off; i < length; i ++) {
+			pms[cnt ++] = unwrap(args[i]);
+		}
+		return pms;
+	}
+	
+	/**
+	 * Javaのオブジェクトとしてアンラップ.
+	 * @param value
+	 * @return
+	 */
+	public static final Object unwrap(Object value) {
+		if(value == null || Undefined.isUndefined(value)) {
+			return value;
+		} else if(value instanceof Wrapper) {
+			return ((Wrapper)value).unwrap();
+		} else if(value instanceof RhiginObjectWrapper) {
+			return ((RhiginObjectWrapper)value).unwrap();
+		} else if (value instanceof IdScriptableObject &&
+			"Date".equals(((IdScriptableObject)value).getClassName())) {
+			// NativeDate.
+			try {
+				// 現状リフレクションで直接取得するようにする.
+				// 本来は ScriptRuntime.toNumber(NativeDate) で取得できるのだけど、
+				// これは rhinoのContextの範囲内でないとエラーになるので.
+				final Method md = value.getClass().getDeclaredMethod("getJSTimeValue");
+				md.setAccessible(true);
+				return JDateObject.newObject(Converter.convertLong(md.invoke(value)));
+			} catch (Exception e) {
+				throw new RhiginException(e);
+			}
+		}
+		return value;
+	}
+	
+	/**
+	 * Javaオブジェクトからrhino向けにラップ.
+	 * @param value
+	 * @return
+	 */
+	public static final Object wrap(Object value) {
+		return wrap(null, value);
+	}
+	
+	/**
+	 * Javaオブジェクトからrhino向けにラップ.
+	 * @param result [0] == trueの場合、変換されました。
+	 * @param value
+	 * @return
+	 */
+	@SuppressWarnings("rawtypes")
+	public static final Object wrap(boolean[] result, Object value) {
+		if(result != null) {
+			result[0] = true;
+		}
+		Class c;
+		if(value == null || Undefined.isUndefined(value)) {
+			return value;
+		} else if(value instanceof Wrapper) {
+			value = ((Wrapper)value).unwrap();
+		} else if(value instanceof RhiginObjectWrapper || value instanceof Scriptable) {
+			return value;
+		}
+		c = value.getClass();
+		if(c.isArray()) {
+			return new JavaScriptable.ReadArray(value);
+		} else if(c.isPrimitive() ||
+			value instanceof Exception ||
+			value instanceof String || value instanceof Boolean || value instanceof Number ||
+			c.getPackage().getName().startsWith(ExecuteScript.RHINO_JS_PACKAGE_NAME)) {
+			return value;
+		} else if (value instanceof java.util.Map) {
+			return new JavaScriptable.GetMap((java.util.Map) value);
+		} else if (value instanceof java.util.List) {
+			return new JavaScriptable.GetList((java.util.List) value);
+		} else if(value instanceof java.util.Date) {
+			return JDateObject.newObject((java.util.Date)value);
+		}
+		if(result != null) {
+			result[0] = false;
+		}
+		return value;
+	}
+
+}
--- project/src/rhigin/scripts/objects/JavaObject.java
+++ project/src/rhigin/scripts/objects/JavaObject.java
@@ -1,23 +1,19 @@
 package rhigin.scripts.objects;
 
-import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.mozilla.javascript.Context;
-import org.mozilla.javascript.IdScriptableObject;
 import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.Undefined;
 import org.mozilla.javascript.Wrapper;
 
-import rhigin.RhiginException;
 import rhigin.scripts.ExecuteScript;
 import rhigin.scripts.JavaScriptable;
 import rhigin.scripts.RhiginFunction;
 import rhigin.scripts.RhiginObject;
 import rhigin.scripts.RhiginObjectWrapper;
-import rhigin.scripts.objects.JDateObject.JDateInstanceObject;
-import rhigin.util.Converter;
+import rhigin.scripts.RhiginWrapUtil;
 import rhigin.util.FastReflect;
 import rhigin.util.FixedKeyValues;
 
@@ -27,75 +23,14 @@ import rhigin.util.FixedKeyValues;
 public class JavaObject {
 	public static final String OBJECT_NAME = "Java";
 	
-	// Javaのパラメータとして処理.
-	private static final Object[] getParams(final int off, final int length, final Object[] args) {
-		int cnt = 0;
-		final Object[] pms = new Object[length - off];
-		for(int i = off; i < length; i ++) {
-			pms[cnt ++] = setObject(args[i]);
-		}
-		return pms;
-	}
-	
-	// javaオブジェクトのフィールド用変換.
-	private static final Object setObject(Object value) {
-		if(value == null) {
-			return value;
-		} else if(value instanceof Wrapper) {
-			return ((Wrapper)value).unwrap();
-		} else if(value instanceof RhiginObjectWrapper) {
-			return ((RhiginObjectWrapper)value).unwrap();
-		} else if (value instanceof JDateInstanceObject) {
-			// JDate.
-			return (JDateInstanceObject)value;
-		} else if (value instanceof IdScriptableObject &&
-			"Date".equals(((IdScriptableObject)value).getClassName())) {
-			// NativeDate.
-			try {
-				// 現状リフレクションで直接取得するようにする.
-				// 本来は ScriptRuntime.toNumber(NativeDate) で取得できるのだけど、
-				// これは rhinoのContextの範囲内でないとエラーになるので.
-				final Method md = value.getClass().getDeclaredMethod("getJSTimeValue");
-				md.setAccessible(true);
-				return JDateObject.newObject(Converter.convertLong(md.invoke(value)));
-			} catch (Exception e) {
-				throw new RhiginException(e);
-			}
+	// 取得されたオブジェクトを変換.
+	private static final Object getObject(Object value) {
+		boolean[] res = new boolean[1];
+		value = RhiginWrapUtil.wrap(res, value);
+		if(!res[0]) {
+			return new JReflectObject(null, value);
 		}
 		return value;
-	}
-	
-	// 取得されたオブジェクトを変換.
-	@SuppressWarnings("rawtypes")
-	private static final Object getObject(Object value) {
-		Class c;
-		if(value == null || value.equals(Undefined.instance)) {
-			return value;
-		} else if(value instanceof Wrapper) {
-			value = ((Wrapper)value).unwrap();
-		} else if(value instanceof RhiginObjectWrapper) {
-			value = ((RhiginObjectWrapper)value).unwrap();
-		}
-		c = value.getClass();
-		if(c.isArray()) {
-			return new JavaScriptable.ReadArray(value);
-		} else if(c.isPrimitive() ||
-			value instanceof Scriptable || value instanceof Exception ||
-			value instanceof String || value instanceof Boolean ||
-			value instanceof Number || value instanceof JReflectObject ||
-			c.getPackage().getName().startsWith(ExecuteScript.RHINO_JS_PACKAGE_NAME)) {
-			return value;
-		} else if (value instanceof java.util.Map) {
-			return new JavaScriptable.GetMap((java.util.Map) value);
-		} else if (value instanceof java.util.List) {
-			return new JavaScriptable.GetList((java.util.List) value);
-		} else if(value instanceof java.util.Date) {
-			if(value instanceof JDateInstanceObject) {
-				return value;
-			}
-			return JDateObject.newObject((java.util.Date)value);
-		}
-		return new JReflectObject(null, value);
 	}
 	
 	// Javaリフレクション制御用オブジェクト.
@@ -122,7 +57,7 @@ public class JavaObject {
 						FastReflect.newInstance(null, (Class)args[0]) :
 						FastReflect.newInstance("" + args[0]);
 				} else {
-					Object[] pms = getParams(1, len, args);
+					Object[] pms = RhiginWrapUtil.unwrapArgs(1, args);
 					o = args[0] instanceof Class ?
 						FastReflect.newInstance(null, (Class)args[0], pms) :
 						FastReflect.newInstance(null, "" + args[0], pms);
@@ -214,7 +149,7 @@ public class JavaObject {
 			if(arg0 != null) {
 				final boolean staticFlag = target == null;
 				if(FastReflect.isField(staticFlag, clazz, arg0)) {
-					FastReflect.setField(clazz, target, arg0, setObject(arg2));
+					FastReflect.setField(clazz, target, arg0, RhiginWrapUtil.unwrap(arg2));
 				}
 			}
 		}
@@ -276,7 +211,7 @@ public class JavaObject {
 			if(len == 0) {
 				return getObject(FastReflect.invoke(clazz, target, name));
 			}
-			Object[] pms = getParams(0, len, args);
+			Object[] pms = RhiginWrapUtil.unwrapArgs(args);
 			return getObject(FastReflect.invoke(clazz, target, name, pms));
 		}
 
